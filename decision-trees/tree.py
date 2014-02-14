292fcc91c2dcc3011ec7210dd9320b8ba01d22fd
Trond Arild Tjostheim <trond@trond-MP061.(none)>
2014-02-14 19:17:45 +0100

    implemented visualize and tree

--- decision-trees/tree.py
+++ decision-trees/tree.py
@@ -3,12 +3,19 @@
 #
 
 class Tree(object):
-	__init__(self):
+	def __init__(self, label):
 		# this is a specific attrbiute - value dictionary
-		self.label = {}
+		self.label = label
 
 		# a dictionary of attribute - tree 
 		self.children = {}
 
 	def add_branch(self, label, subtree):
 		self.children[label] = subtree
+
+	def __repr__(self):
+		# return a string with the tree
+		retstr = str(self.label) 
+		for key, value in self.children:
+			retstr+='; '+str(value)
+		return retstr
--- decision-trees/visualize.py
+++ decision-trees/visualize.py
@@ -1,6 +1,31 @@
 #
 # somehow displays a tree
-$
+#
 def visualize(tree):
 	# todo
 	# print out with tabs
+	def tree_to_str(tree, parentstr):
+		
+		retstr = parentstr + str(tree.label) + '\n'
+		if len(tree.children)==0:
+			return retstr
+
+		for key, value in tree.children.iteritems() :
+			 retstr += tree_to_str(value, parentstr+'|\t')
+		
+		return retstr
+
+
+	print tree_to_str(tree, '')
+
+	'''
+	retstr = self.label[0] + ' == ' + self.label[1]
+		if len(self.children) > 0:
+			retstr += 'num children= ' + str(len(self.children)) + ' \n'
+			#retstr += '|\t'
+			for key, value in self.children.iteritems():
+				retstr +=  '|\t' +str(value) 
+		else:
+			retstr+='\n'
+		return retstr
+	'''
