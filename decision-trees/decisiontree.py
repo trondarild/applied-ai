b1b4207c920d3b06d38e0b4ebdc452cce878ea88
Trond Arild Tjostheim <trond@trond-MP061.(none)>
2014-02-15 17:01:22 +0100

    fixed errors so should now be ready to test with importance() and plurality_value()

--- decision-trees/decisiontree.py
+++ decision-trees/decisiontree.py
@@ -2,46 +2,59 @@
 # Decision tree algorithm
 #
 from utils import argmax
-import plurality-value
-import importance
-import tree
+from pluralityvalue import *
+from importance import *
+from tree import *
 
 
-def decision-tree-learning(examples, attributes, parent_examples):
+def decision_tree_learning(examples, attributes, parent_examples):
+	
+	#
+	# Checks if the given examples have the same classification
+	# examples: [{exampledictionary}, 'class']
+	# return: pair with (true/false, the classification)
 	def is_same(examples):
+		if len(examples) == 0:
+			return (False, None)
+
 		classification = []
-		classification.append(examples[0][1])
-		isSame = True
+		class_instance = examples[0][1]
+		classification.append(class_instance)
+		#isSame = True
 		for example in examples[1:] :
-			if example[1] in classication:
+			if example[1] in classification:
 				continue
 			else:
 				return (False, None)
 				
-		return (True, classication[0])
+		return (True, classification[0])
 
+	def remove_dict_entry(key, dictionary):
+		retval = dict(dictionary)
+		del retval[key]
+		return retval
 	
 	sameclass,classification = is_same(examples)
 
-	if len(examples)==0:
-		return plurality-value(parent_examples)
-	elif sameclass: return classification
-	elif len(attributes)==0: return plurality-value(examples)
+	if len(examples)==0: return Tree(plurality_value(parent_examples))
+	elif sameclass: return Tree(classification)
+	elif len(attributes)==0: return Tree(plurality_value(examples))
 	else:
-		attribute = argmax(attributes, lambda ((a, e)): importance(a, e))
-		tree = Tree(attribute)
+		
+		attributename = argmax(attributes.keys(), lambda ((a)): importance(a, examples))
+		tree = Tree(attributename)
 		exs = []
-		for vk in attribute.value:
+		for vk in attributes[attributename]:
 			for example in examples:
 				exvalues = example[0]
-				if exvalues[attribute.key] == vk:
+				if exvalues[attributename] == vk:
 					exs.append(example)
 
-			newattributes = attributes.remove(attribute)#remove_member(attribute, attibutes)
-			subtree = decision-tree-learning(exs, newattributes, examples)
+			newattributes = remove_dict_entry(attributename, attributes) #remove_member(attribute, attibutes)
+			subtree = decision_tree_learning(exs, newattributes, examples)
 			# make a label by combining attribute name with a spacific
 			# attribute value
-			label = {attribute.key:vk} 
+			label = vk #{attribute.key:vk} 
 			tree.add_branch(label, subtree)
 
 		return tree
