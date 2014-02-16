a6bc381611f837889462a5483989566ca09d89e9
Trond Arild Tjostheim <trond@trond-MP061.(none)>
2014-02-16 10:14:42 +0100

    fixed bug in visualize so prints out attribute names and values properly

--- decision-trees/visualize.py
+++ decision-trees/visualize.py
@@ -2,30 +2,34 @@
 # somehow displays a tree
 #
 def visualize(tree):
-	# todo
-	# print out with tabs
+	'''
+	print out tree with tabs, such that children are one tab 
+	away from parent, recursively
+	
+	tree: subtree to print
+	parentstr: a series of '|\t', which places the output at the correct distance
+	'''
 	def tree_to_str(tree, parentstr):
+		# get the label, i.e. attribute name, and add it to parentstr
+		# will need this later to print out children
+		labelstr = parentstr + str(tree.label)
 		
-		retstr = parentstr + str(tree.label) + '\n'
+		# if have no children, just print out the label, 
+		# it should be one of the final class, i.e. yes or no
 		if len(tree.children)==0:
-			return retstr
+			return labelstr +'\n'
 
+		# recursive calls through children. For each child
+		# print out own attribute and the attribute's value
+		# then print out every branch corresponding to that
+		# attribute value
+		retstr=''
 		for key, value in tree.children.iteritems() :
-			 retstr += tree_to_str(value, parentstr+'|\t')
+			retstr += labelstr + '==' + key + ':\n' 
+			retstr += tree_to_str(value, parentstr + '|\t')
 		
 		return retstr
 
 
 	print tree_to_str(tree, '')
 
-	'''
-	retstr = self.label[0] + ' == ' + self.label[1]
-		if len(self.children) > 0:
-			retstr += 'num children= ' + str(len(self.children)) + ' \n'
-			#retstr += '|\t'
-			for key, value in self.children.iteritems():
-				retstr +=  '|\t' +str(value) 
-		else:
-			retstr+='\n'
-		return retstr
-	'''
