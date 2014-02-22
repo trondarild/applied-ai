de0f2bd1c77fa9a369058ba41c328de48cd4ce21
Trond Arild Tjostheim <trond@trond-MP061.(none)>
2014-02-22 21:22:11 +0100

    Made code more readable by removing casts to float

--- decision-trees/importance.py
+++ decision-trees/importance.py
@@ -8,18 +8,19 @@ def Remainder(attribute,examples,attributes,p,n,classes_list):
 
 	sum = 0
 	for v in attributes[attribute]:
-		pi=0
-		ni=0
+		pi=0.0
+		ni=0.0
 		for example in examples:
 			if example[0][attribute]==v:
 				if example[1]==classes_list[0]:
-					pi+=1
+					pi+=1.0
 				else:
-					ni+=1
+					ni+=1.0
 
-		if pi == 0 and ni == 0:
+		if pi == 0.0 and ni == 0.0:
 			continue
-		sum+=float(pi+ni)/float(p+n)*I(float(pi)/float(pi+ni),float(ni)/float(pi+ni))
+		#sum+=float(pi+ni)/float(p+n) * I( float(pi)/float(pi+ni), float(ni)/float(pi+ni) )
+		sum += (pi+ni)/(p+n) * I( pi/(pi+ni), ni/(pi+ni) )
 
 	return sum
 
@@ -27,13 +28,13 @@ def I(x,y):
 	if x==0 or y==0 :
 		return 0
 	else :
-		return float(-x*math.log(x,2)-y*math.log(y,2))
+		return -x*math.log(x,2)-y*math.log(y,2)
 
 
 def importance(attribute, examples, attributes,classes_list):
 	# todo
-	p = 0
-	n = 0
+	p = 0.0
+	n = 0.0
 	for example in examples:
 		if example[1] == classes_list[0]:
 			p += 1
@@ -43,12 +44,19 @@ def importance(attribute, examples, attributes,classes_list):
 	if p==0 and n==0:
 		print "Error:no examples to calculate importance"
 	#print attribute, I(float(p)/float(p+n),float(n)/float(p+n)) - Remainder(attribute,examples,attributes,p,n)
-	gain = I(float(p)/float(p+n),float(n)/float(p+n))-Remainder(attribute,examples,attributes,p,n,classes_list)
+	gain = I(p/(p+n),n/(p+n))-Remainder(attribute,examples,attributes,p,n,classes_list)
 	if gain<1.0e-15: gain=0
 	return gain
 
+import operator
 
 if __name__ == '__main__':
 	examples,attributes,classification = readarff.read_arff('restaurant-domain.arff')
 	#print I(1.0/3,2.0/3)
-	print importance('Type',examples,attributes)
+	print attributes
+	infgains={}
+	for attr, attval in attributes.iteritems():
+		infgains[attr] = importance(attr ,examples,attributes, classification)
+	sorted_x = sorted(infgains.iteritems(), key=operator.itemgetter(1))
+		#print attr +': ' + str(importance(attr ,examples,attributes, classification))
+	for i in sorted_x: print i
