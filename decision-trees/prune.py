35e0667d76f268c4946706960f28e20a173f6095
Trond Arild Tjostheim <trond@trond-MP061.(none)>
2014-02-22 21:21:34 +0100

    Simplified code

--- decision-trees/prune.py
+++ decision-trees/prune.py
@@ -4,11 +4,6 @@ from collections import Counter
 from tree import Tree
 
 
-def calc_chisquared(a, b):
-    if b>0 :
-	    return pow((a-b), 2)/b
-    return 0.0
-
 #
 # collect statistics data for attributes and attribute vales
 #
@@ -18,7 +13,6 @@ def get_stat_data(examples, attributes, classes):
 	classcount = {}
 	attrentropy = {}
 	totalclasscount = {class1:0.0, class2:0.0}
-	informationgain = {}
 
 	# initialize data structure
 	for attname, attvalues in attributes.items():
@@ -31,7 +25,6 @@ def get_stat_data(examples, attributes, classes):
 			classcount[attname][attvalue][class2]=0.0
 			attrentropy[attname][attvalue] = 0.0
 
-
 	# go through examples, and for each attribute,
 	# count the number of classifications (yes/no)
 	for examplerow in examples:
@@ -41,6 +34,7 @@ def get_stat_data(examples, attributes, classes):
 		for attname, attvalue in example.items():
 			classcount[attname][attvalue][examplerow[1]] += 1
 	return classcount
+
 #
 # do chi squared statistical test on observed data, compare with
 # expected. Assume both lists same length
@@ -79,24 +73,20 @@ def get_expected_values(examples, attributes, classes):
 		...			-----------------------------------------
 					 sum2			| sum3			| sum4=sum0+sum1
 
-		chi squared
+		chi squared (not used here)
 					|yesclass 		| noclass 		|
 		valuename0	|(a-a')^2/a'   	| (b-b')^2/b'  	|
 		valuename1	|(c-c')^2/c'  	| (d-d')^2/d'  	|
 		...			-----------------------------------------
 
-		totalchisquared = chisq00 + chisq01 + chisq10 + chisq11 ..
-		degrees of freedom: df = numvaluenames-1
-		chicheck: chisquaredtable[df] < totalchisquared ?
 
-		return {attribute:chiceck}, {attribute:{attvalue:[expectedvals]}}
+		return expectedvals
 
 	expectedpos[attribute] = totalpositive* (positive[attribute]+negative[attribute])
 	# check against the chi squared table
 	'''
 	class1 = classes[0]
 	class2 = classes[1]
-	retval = {}
 	expectedvals = {}
 	for attributename, attributevalues in statdata.iteritems():
 		sumarray = []
@@ -117,7 +107,6 @@ def get_expected_values(examples, attributes, classes):
 		expectedvals[attributename] = attvalueexpvals
 
 		# calculate expected values, and the sum of chisquared
-		chisquaredsum = 0
 		sum4 = sum(sumarray)
 		i=0
 		for attributevalue, classes in attributevalues.iteritems():
@@ -157,67 +146,46 @@ def irrelevance_evaluation(tree, expectedvalues, classification):
 		leaf = c.most_common(1)[0][0]
 		return leaf
 
-'''
-tsttree = Tree('type')
-tsttree.add_branch('italian',Tree('No'))
-tsttree.add_branch('french',Tree('No'))
-tsttree.add_branch('burger',Tree('No'))
-tsttree.add_branch('thai',Tree('Yes'))
-expvals = {'italian':[2,2], 'french':[2,2], 'burger':[1,1], 'thai':[1,1]}
-cls = ['Yes', 'No']
-#print tsttree.children
-print irrelevance_evaluation(tsttree, expvals, cls)
-'''
 
 #
 # Prunes the tree
 # tree - the tree to prune
 # examples - list of examples used to calculate entropy gain
 # attributes - list of attributes used to calculate entropy gain
+# classification - a list of 2 classification names ['class1', 'class2'] 
+# expectedvals - dictionary of 
+#	attributename:{attributevaluename:[expectedclass1, expectedclass2]..}..}
 # returns a pruned tree
 #
 def prune(tree, examples, attributes, classification, expectedvals):
 	# start with complete tree
 	# eliminate irrelevant nodes:
-	# look at test node with *only* leaf nodes as children
-	# if test gives irrelevance (how  to check this?) - only noise in data - eliminate and replace with leaf
-	# do this for every test with leaf nodes
-	# repeat
+	# 		look at test node with *only* leaf nodes as children
+	# 		if test gives irrelevance (how  to check this?) - only noise in data - eliminate and replace with leaf
+	# 		do this for every test with leaf nodes
+	# 		repeat
 	# irrelevance test: information gain close to zero
 
-
+	# get a list of children nodes which themselves have children|
 	def get_branch_list(children):
 		retval = []
 		for key, branch in children.iteritems():
-			if len(branch.children) != 0:
+			if len(branch.children) > 0:
 				retval.append(key)
 		return retval
-
-    # get expected values for leaves
- 	# expectedvals = get_expected_values(examples, attributes, classification)
 
 	children = tree.children
 	branchnames = get_branch_list(children)
 	# check if all children are leaves
 	# (note this could be done with a lambda applied on set of all children)
-
+	#retval = Tree()
 	# if no branches, only leaves, do irrelevance evaluation
-	if  len(branchnames) == 0 :
-		# do irrelevance evaluation
-		return irrelevance_evaluation(tree, expectedvals[tree.label], classification)
 
-	else:
-		# do recursive call on  branches that have children
+	if len(branchnames) > 0:
 		for branchname in branchnames:
 			children[branchname] = prune(children[branchname], examples, attributes, classification, expectedvals)
-
-		# check if now have only leaves
-		branchnames = get_branch_list(children)
-		'''
-		# note: ugly repetition, can it be removed?
-		'''
-		if  len(branchnames) == 0 :
-			# do irrelevance evaluation
-			return irrelevance_evaluation(tree, expectedvals[tree.label], classification)
-		else:
+		# if pruning returned branches, just return the tree
+		if len(get_branch_list(children))>0:
 			return tree
+
+	return irrelevance_evaluation(tree, expectedvals[tree.label], classification)
