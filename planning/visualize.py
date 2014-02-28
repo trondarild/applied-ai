f3939b12ce12bc649ce9c1defd6d405da9114127
trondarild <trondarild@gmail.com>
2014-02-28 13:40:17 +0100

    Added function to visualize board

---
+++ planning/visualize.py
@@ -0,0 +1,12 @@
+
+'''
+Visualizes the wumpus world
+size - pair of ints (x, y) - dimensions of world
+agentpos- pair of ints (x, y) - position of player
+goldpos - pair of ints (x, y) - position of gold
+wumpuses - list of pair of ints [(x,y), ..] - position of wumpuses
+pits - list of pair of ints [(x,y),..] - position of pits
+'''
+def visualize (size, agentpos, goldpos, wumpuses, pits) :
+	# todo
+	return 0
