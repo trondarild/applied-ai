3b2174001d6cfff2204f5c087b4b04cc827aeb1b
lizuyao2010 <zuyaoli2010@gmail.com>
2014-03-05 10:02:07 +0100

    add read_corpus.py

---
+++ nlp/read_corpus.py
@@ -0,0 +1,65 @@
+
+def tag1_tag2_prob(tag1, tag2,dicOfTag, dicOfTags):
+	if dicOfTag.has_key(tag1):
+		denominator=dicOfTag[tag1]
+	else:
+		denominator=0
+		print "no answer"
+		return
+	if dicOfTags.has_key((tag1,tag2)):
+		numerator=dicOfTags[(tag1,tag2)]
+	else:
+		numerator=0
+	return float(numerator)/denominator
+
+def word_tag_prob(tag, word,dicOfWordTag, dicOfTag):
+	if dicOfWordTag.has_key((word,tag)):
+		numerator=dicOfWordTag[(word,tag)]
+	else:
+		numerator=0
+	if dicOfTag.has_key(tag):
+		denominator=dicOfTag[tag]
+	else:
+		denominator=0
+		print "no answer"
+		return
+	return float(numerator)/denominator
+
+def read_corpus(fileName):
+	dicOfWordTag=dict()
+	dicOfTag=dict()
+	dicOfTags=dict()
+	f=open(fileName,"r")
+	prevTag=''
+	for line in f:
+		line=line.strip()
+		if line=='':
+			prevTag=''
+			None
+		else:
+			line=line.split('\t')
+			word=line[1].strip()
+			tag=line[4].strip()
+			wordtag=(word,tag)
+			if dicOfWordTag.has_key(wordtag):
+				dicOfWordTag[wordtag]+=1
+			else:
+				dicOfWordTag[wordtag]=1
+			if dicOfTag.has_key(tag):
+				dicOfTag[tag]+=1
+			else:
+				dicOfTag[tag]=1
+			if prevTag:
+				tags=(prevTag,tag)
+				if dicOfTags.has_key(tags):
+					dicOfTags[tags]+=1
+				else:
+					dicOfTags[tags]=1
+			prevTag=tag
+
+	return dicOfWordTag,dicOfTag,dicOfTags
+
+if __name__ == '__main__':
+	dicOfWordTag,dicOfTag,dicOfTags=read_corpus("CoNLL2009-ST-English-train-pos.txt")
+	print tag1_tag2_prob('JJ', 'NN', dicOfTag, dicOfTags)
+	print word_tag_prob('JJ', 'round', dicOfWordTag, dicOfTag)
